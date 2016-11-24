5ae1d7bbb3fccf28b1403299b2b255a4eb3590ba
cosineyu <yzycn@qq.com>
2016-11-24 16:53:19 +0800

    f

--- dht/dht.go
+++ dht/dht.go
@@ -1,27 +1,65 @@
 package dht
 
 import "net"
+import (
+    "github.com/zeebo/bencode"
+    "fmt"
+)
 
 type DHTNode struct{
     ID string
-    Ip string
-    Port uint16
+    Address net.UDPAddr
 }
 
 type DHTServer struct{
     ID string
     Conn net.UDPConn
+    IP string;
+    Port uint16;
 }
 
-func (this *DHTServer) SendMsg(address string, msg interface{}) error {
+func (this *DHTServer) sendMsg(address net.UDPAddr, msg interface{}) error {
+    data, err := bencode.EncodeBytes(msg)
+    if err != nil {
+        return err
+    }
+    fmt.Println(data)
+    _, err = DHTServer.Conn.WriteToUDP(data, address)
+    return err
+}
 
-    DHTServer.Conn.Write()
+func (this *DHTServer) recvMsg(address net.UDPAddr, msg interface{}) error {
+    krcpMsg := msg.(map[string]interface{})
+    krcpMsg["r"]
+    return nil
 }
 
 func (this *DHTServer) findNode(target string) error {
-
+    return nil
 }
 
-func getP()  {
-    
+func (this *DHTServer) Run() {
+    addr, err := net.ResolveUDPAddr("udp", this.IP + ":" + this.Port)
+    if err != nil {
+        fmt.Println("net.ResolveUDPAddr fail.", err)
+        return
+    }
+
+    this.Conn, err = net.DialUDP("udp", nil, addr)
+    if err != nil {
+        fmt.Println("net.DialUDP fail.", err)
+        return
+    }
+    defer this.Conn.Close()
+    var Buff [65535]byte;
+    for {
+        rlen, remote, err := this.Conn.ReadFromUDP(Buff)
+        if err == nil {
+            var msg interface{}
+            bencode.DecodeBytes(Buff[:rlen], msg)
+            this.recvMsg(remote, msg)
+        }
+    }
 }
+
+
