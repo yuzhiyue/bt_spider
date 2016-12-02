b845b8df3e929e4e8ce649b098812fa4813e757d
cosineyu <yzycn@qq.com>
2016-12-02 17:49:22 +0800

    f

--- dht/dht.go
+++ dht/dht.go
@@ -5,6 +5,8 @@ import (
     "github.com/zeebo/bencode"
     "fmt"
     "strconv"
+    "bytes"
+    "encoding/binary"
 )
 
 type DHTNode struct{
@@ -78,10 +80,11 @@ func (this *DHTServer) recvMsg(address net.UDPAddr, msg interface{}) error {
 
     if y == "q" {
         q := krcpMsg["q"].(string)
+        a := krcpMsg["a"].(map[string]interface{})
         if q == "ping" {
 
         } else if q == "find_node" {
-
+            this.onFindNodeRequest(address, t, a)
         } else if q == "get_peers" {
 
         } else if q == "announce_peer" {
@@ -91,10 +94,11 @@ func (this *DHTServer) recvMsg(address net.UDPAddr, msg interface{}) error {
         tranData := this.transMap[t]
         reqMsg := tranData.Data
         q := reqMsg["q"].(string)
+        r := krcpMsg["r"].(map[string]interface{})
         if q == "ping" {
 
         } else if q == "find_node" {
-
+            this.onFindNodeResponse(address, r)
         } else if q == "get_peers" {
 
         } else if q == "announce_peer" {
@@ -111,9 +115,36 @@ func (this *DHTServer) findNode(addr net.UDPAddr, target string) error {
     return nil
 }
 
-func (this *DHTServer) onFindNode(addr net.UDPAddr, t string, msg map[string]interface{})  {
+func (this *DHTServer) onFindNodeRequest(addr net.UDPAddr, t string, msg map[string]interface{})  {
     rspMsg := map[string]interface{} {"id":this.ID, "nodes": ""}
     this.sendMsg(addr, t, "r", nil, rspMsg)
+}
+
+func inet_ntoa(ipnr int64) string {
+    var bytes [4]byte
+    bytes[0] = byte(ipnr & 0xFF)
+    bytes[1] = byte((ipnr >> 8) & 0xFF)
+    bytes[2] = byte((ipnr >> 16) & 0xFF)
+    bytes[3] = byte((ipnr >> 24) & 0xFF)
+
+    return net.IPv4(bytes[3],bytes[2],bytes[1],bytes[0]).String()
+}
+
+func (this * DHTServer) onFindNodeResponse(addr net.UDPAddr, msg map[string]interface{})  {
+    nodes := msg["nodes"].(string)
+    if len(nodes) % 26 != 0 {
+        return
+    }
+    reader := bytes.NewReader(nodes)
+    for i := 0; i < len(nodes); i += 26 {
+        var id [20]byte
+        var ip uint32
+        var port uint16
+        binary.Read(reader, binary.BigEndian, &id)
+        binary.Read(reader, binary.BigEndian, &ip)
+        binary.Read(reader, binary.BigEndian, &port)
+        fmt.Println("new node:", string(id), inet_ntoa(ip), port)
+    }
 }
 
 func (this *DHTServer) Run() {
@@ -129,4 +160,21 @@ func (this *DHTServer) Run() {
     }
 }
 
+func main()  {
+    s := DHTServer{}
+    s.ID = "mnopqrstuvwxyz123456"
+    s.IP = "0.0.0.0"
+    s.Port = "12000"
+    s.Init()
+    ns, _ := net.LookupHost("router.bittorrent.com")
+    ip := ns[0]
+    fmt.Println(ip)
+    addr, err := net.ResolveUDPAddr("udp", ip + ":" + 6881)
+    if err != nil {
+        fmt.Println("net.ResolveUDPAddr fail.", err)
+        return
+    }
+    s.findNode(addr, s.ID)
+    s.Run()
+}
 
